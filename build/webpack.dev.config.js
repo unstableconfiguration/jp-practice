976282258cf4b2f8a0b30036aeb939e59a9d865d
oldmanmuffins <mluper.official@protonmail.com>
2021-02-09 15:37:13 -0500

    refining build

---
+++ build/webpack.dev.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    mode : 'development',
+    devtool : false,
+    entry : {
+        index : {
+            import : './src/index.js'
+        }
+    },
+    output : {
+        filename : '[name].js',
+        path : path.resolve(__dirname, '../dist')
+    },
+    plugins : [
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin({
+            patterns : [
+                { 
+                    from : 'src', 
+                    filter : (path) => { 
+                        return path.substr(-4) === 'html'
+                            || path.substr(-3) === 'css'
+                    } 
+                },
+            ]
+        })
+    ]
+};
